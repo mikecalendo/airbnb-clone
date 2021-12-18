6ea58dbdd081a64e3ae94ef33eecc2cab2013635
mikecalendo <contact@michaelcalendo.com>
2021-12-18 08:08:45 -0800

    0808A121821

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+function Footer() {
+  return (
+    <div
+      className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32
+          py-14 bg-gray-800 text-gray-100"
+    >
+      <div className="space-y-4 text-xs text-gray-100">
+        <h5 className="font-bold">TITLE</h5>
+        <p>Like </p>
+        <p>Currently</p>
+        <p>Full-Stack,</p>
+        <p>The Valley,</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-100">
+        <h5 className="font-bold">GALLERY</h5>
+        <p>What</p>
+        <p>Looking For</p>
+        <p>Web Developer.</p>
+        <p>AZ</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-100">
+        <h5 className="font-bold">LOCATIONS</h5>
+        <p>You</p>
+        <p>A Good</p>
+        <p>Are You</p>
+        <p>Mikey C.</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-100">
+        <h5 className="font-bold">CONTACT</h5>
+        <p>See?</p>
+        <p>Developer Job.</p>
+        <p>Hiring?</p>
+        <p>contact@michaelcalendo.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
